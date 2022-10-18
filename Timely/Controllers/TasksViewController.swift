2c0ab2fb9a6619cc24a55e4300c89de0835a28aa
Cole Stevens <cpsmobiledev@gmail.com>
2022-10-18 10:12:42 -0600

    Initial App Framework

--- Timely/Controllers/TasksViewController.swift
+++ Timely/Controllers/TasksViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ItemArray = [ToDo]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
 
 }
 
---
+++ Timely/Models/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  Timely
+//
+//  Created by cole stevens on 9/25/22.
+//
+
+import Foundation
